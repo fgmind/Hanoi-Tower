5bd2e9dbe7c2ec591cac6e42974628b44692126f
Francois <20587355+fgmind@users.noreply.github.com>
2018-01-31 13:29:56 +1300

    Add files via upload

---
+++ HanoiTower/HanoiTower/HanoiTower.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HanoiTower
+{
+    class HanoiTower        //class containing method to move discs from 1 peg to the other
+    {
+     
+        public void moveDiscs(int n, String a, String b, String c)
+        {
+            if (n == 1)
+            {
+                Console.WriteLine("Move Disc from " + a + " to " + b);
+            }
+            else
+            {
+                moveDiscs(n - 1, a, c, b);                              // reloads method and decrements n, also swaps pegs
+
+                Console.WriteLine("Move Disc from " + a + " to " + b);
+                moveDiscs(n - 1, c, b, a);                              // will loop through method until n = 1
+
+            }
+        }
+    }
+}
---
+++ HanoiTower/HanoiTower/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HanoiTower
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HanoiTower hanoi = new HanoiTower();
+
+            Console.WriteLine("Enter number of discs: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            hanoi.moveDiscs(n, "Peg A", "Peg C", "Peg B");  // had to swap Peg B and C...
+        }
+    }
+}
